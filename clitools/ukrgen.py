e5cf6246371825e4e71c67fe08dce665b4908713
linedot <linedot@xcpp.org>
2025-08-24 15:19:30 +0200

    debug output: aliased VEC regs

--- clitools/ukrgen.py
+++ clitools/ukrgen.py
@@ -657,6 +657,10 @@ def main():
     for alias,regidx in rt.aliased_regs['greg'].items():
         genlog.debug(f"  {alias:30} : {gen.greg(regidx)}")
 
+    genlog.debug("Aliased VEC regs in the end:")
+    for alias,regidx in rt.aliased_regs['vreg'].items():
+        genlog.debug(f"  {alias:30} : {gen.vreg(regidx)}")
+
 
     inout_args,rest = parse_inout_args(parser=parser)
 
