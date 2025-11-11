cb8112b49de289e25c597f0c2717cf01f557c790
linedot <linedot@xcpp.org>
2025-11-11 10:55:07 +0100

    Add gemm_context

---
+++ src/ukrgen/composers/gemm.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from asmgen.registers import asm_data_type as adt
+from ..specializers.asm import lsc_specializer,op_support
+
+
+class gemm_context:
+    def __init__(self):
+        self.gen : asmgen = None
+        self.rt : register_tracker = None
+        self.params : dict[str,str] = dict()
+        self.specializer : lsc_specializer = None
+        self.op_support_list : list[op_support] = list()
+        self.sup : op_support = None
+
+
+class blis_composer:
+
+
+    pass
