d40722b259d7cb8674946a18bf4f09d6a2b19fb6
linedot <linedot@xcpp.org>
2025-11-13 13:26:02 +0100

    Add SPDX header to some new files

--- src/ukrgen/composers/gemm.py
+++ src/ukrgen/composers/gemm.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT OR GPL-3.0-or-later
+# Copyright (C) 2021 Stepan Nassyr <s.nassyr@fz-juelich.de>
+# Copyright (C) 2021 Stepan Nassyr <s.nassyr@xcpp.org>
+# ------------------------------------------------------------------------------
+
 from dataclasses import dataclass
 from asmgen.registers import asm_data_type as adt
 from ..specializers.asm import lsc_specializer,op_support
--- src/ukrgen/composers/stages/datatype.py
+++ src/ukrgen/composers/stages/datatype.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT OR GPL-3.0-or-later
+# Copyright (C) 2021 Stepan Nassyr <s.nassyr@fz-juelich.de>
+# Copyright (C) 2021 Stepan Nassyr <s.nassyr@xcpp.org>
+# ------------------------------------------------------------------------------
+
 from asmgen.registers import asm_data_type as adt
 
 from .composition import composition_stage
